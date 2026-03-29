bb10446f2caff3da6b126c89468762023416cfc6
JonJagger <jon@kosli.com>
2026-03-29 14:46:20 +0100

    Switch to github workflow, move red-amber-green to start-point

---
+++ docker/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -Eeu
+
+mkdir cdl && cd cdl
+
+cat > go.mod << 'EOF'
+module cdl-go-convey
+
+go 1.26.1
+
+require github.com/smartystreets/goconvey
+EOF
+
+# Dummy file so go mod tidy and go build include testify/assert
+cat > dummy.go << 'EOF'
+package cdl
+import _ "github.com/smartystreets/goconvey/convey"
+EOF
+
+# Resolve all deps (updates go.mod with indirect deps and populates go.sum)
+go mod tidy
+
+# Pre-compile testify into a shared build cache accessible by all users
+mkdir /go/build-cache
+GOCACHE=/go/build-cache go build ./...
+chmod -R 777 /go/build-cache
